625803ca2c66e6ae2fb9b5015b73831311ccc27b
BrunoZimmer <bruno.s.zimmer@hotmail.com>
2021-11-06 20:49:26 +0100

    corrections errors sur le github

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 $(window).on("scroll", function() {
     var pageScroll = $(window).scrollTop();
 
-    if(pageScroll < 150) {
+    if(pageScroll < 250) {
         $(".DivNavHeaderAccueil").addClass("active");
         $(".DivNavHeaderTrombinoscope").removeClass("active");
-    } else if (pageScroll > 150 && pageScroll < 250 ){
+    } else if (pageScroll > 250 && pageScroll < 350 ){
         //remove the background property so it comes transparent again (defined in your css)
        $(".DivNavHeaderAccueil").removeClass("active");
        $(".DivNavHeaderTrombinoscope").addClass("active");
