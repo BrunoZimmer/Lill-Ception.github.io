4318230837e67dc11f50b30797353bd0b68b7e8a
BrunoZimmer <bruno.s.zimmer@hotmail.com>
2021-11-07 16:29:29 +0100

    trombi avec les errors

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 $(window).on("scroll", function() {
     var pageScroll = $(window).scrollTop();
 
-    if(pageScroll < 150) {
+    if(pageScroll < 250) {
         $(".DivNavHeaderTrombinoscope").removeClass("active");
 
         $(".DivNavHeaderAccueil").addClass("active");
-    } else if (pageScroll > 150 && pageScroll < 350 ){
+    } else if (pageScroll > 250 && pageScroll < 350 ){
        $(".DivNavHeaderProgramme").removeClass("active");
        $(".DivNavHeaderAccueil").removeClass("active");
 
@@ -17,19 +17,46 @@ $(window).on("scroll", function() {
         $(".DivNavHeaderVideos").removeClass("active");
 
         $(".DivNavHeaderProgramme").addClass("active");
-    } else if (pageScroll > 450 && pageScroll < 550 ){
-        $(".DivNavHeaderProgramme").removeClass("active");
-        $(".DivNavHeaderEvenements").removeClass("active");
+    } else if (pageScroll > 350 && pageScroll < 450 ){
+        $(".DivNavHeaderTrombinoscope").removeClass("active");
+        $(".DivNavHeaderVideos").removeClass("active");
 
         $(".DivNavHeaderVideos").addClass("active");
-    } else if (pageScroll > 550 && pageScroll < 650 ){
+    } else if (pageScroll > 350 && pageScroll < 450 ){
+        $(".DivNavHeaderTrombinoscope").removeClass("active");
         $(".DivNavHeaderVideos").removeClass("active");
-        $(".DivNavHeaderSponsors").removeClass("active");
 
-        $(".DivNavHeaderEvenements").addClass("active");
-    }  else if (pageScroll > 650){
-        $(".DivNavHeaderEvenements").removeClass("active");
+        $(".DivNavHeaderVideos").addClass("active");
+    } else if (pageScroll > 350 && pageScroll < 450 ){
+        $(".DivNavHeaderTrombinoscope").removeClass("active");
+        $(".DivNavHeaderVideos").removeClass("active");
         
-        $(".DivNavHeaderSponsors").addClass("active");
+        $(".DivNavHeaderVideos").addClass("active");
+    } 
+});
+
+
+$(window).on("scroll", function() {
+    var pageScroll = $(window).scrollTop();
+
+    if(pageScroll < 50) {
+        $(".TitreImage").removeClass("LogoImageActive");
+        $(".TitreImage").addClass("LogoImageInactive");
+
+        $(".LogoImage").removeClass("LogoImageInactive");
+        $(".LogoImage").addClass("LogoImageActive");
+
+        $("#navIcons").removeClass("NavIcons");
+        $("#navIcons").addClass("NavIconsInit");
+        width="130"
+    } else{
+        $(".TitreImage").removeClass("LogoImageInactive");
+        $(".TitreImage").addClass("LogoImageActive");
+
+        $(".LogoImage").removeClass("LogoImageActive");
+        $(".LogoImage").addClass("LogoImageInactive");
+
+        $("#navIcons").removeClass("NavIconsInit");
+        $("#navIcons").addClass("NavIcons");
     } 
 });
